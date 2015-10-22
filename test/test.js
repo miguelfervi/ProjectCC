bcf5fea542902a0044fbe692af40b9b5f40238a5
JCristobal <tobas92@gmail.com>
2015-10-23 00:50:31 +0200

    Añado test de mocha básicos

--- test/test.js
+++ test/test.js
@@ -1,10 +1,52 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
+
+objCalculator = {
+  addNumber: function(a, b){
+    return a + b;
+  },
+
+  substractNumber: function(a, b){
+    return a - b;
+  },
+
+  multiplyNumber: function(a, b){
+    return a * b;
+  },
+
+  divideNumber: function(a, b){
+    return a / b;
+  }
+}
+
+
+describe('Test básicos', function() {
+
+  describe('Elementos con indexOf()', function () {
+    it('basic test: should return -1 when the value is not present', function (done) {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+      done();
     });
   });
+
+
+
+  describe('Operaciones básicas con fucnciones locales', function () {
+    it('Sumamos 2 y 3', function (done) {
+      assert.equal(5, objCalculator.addNumber(2, 3));
+      done();
+    });
+    it('Multiplicamos 2 y 2', function (done) {
+      assert.equal(4, objCalculator.multiplyNumber(2, 2));
+      done();
+    });
+    it('Dividimos 3 y 1', function (done) {
+      assert.equal(3, objCalculator.divideNumber(3, 1));
+      done();
+    });
+  });
+
+
+
 });
 
