db710a7f1f76ec4cb71632c2a3c15287bf8fda3a
Miguel Fernández Villegas <miguelfervi@gmail.com>
2015-11-23 11:12:53 +0100

    Eliino test DB

--- test/test.py
+++ test/test.py
@@ -27,56 +27,34 @@ def test_dbcon():
 	return db==client.usuarios
 
 class test (unittest.TestCase):
-	# Inserción de un usuario en la base de datos
-	def test_1(self):
-		post = {"user": "miguel",
-		    	"nombre": "miguel",
-		    	"apellidos": "fernandez",
-		    	"correo": "miguel@correo.es",
-		    	"dia": "5",
-		    	"mes": "7",
-		    	"anio": "1991",
-			"direccion": "Calle secreta prueba",
-		    	"password": "123456",
-		    	"pago": "VISA",
-		    	"visa": "1111-1111-1111-1111",
-		    	}
-
-		posts=db.posts
-		post_id = posts.insert(post)    
-
-		#Comprobamos si realmente se ha insertado en la base de datos
-		query=posts.find({"user": "miguel"})
-		return query.count() == 1
-
-	    def test_2(self):	
+	
+	    def test_1(self):	
 		existe_clase(registro) 
 	
-	    def test_3(self):	
+	    def test_2(self):	
 		existe_clase(login)
 	
-	    def test_4(self):	
+	    def test_3(self):	
 		existe_clase(logout)
 	
-	    def test_5(self):	
+	    def test_4(self):	
 		existe_clase(ver_perfil)
 	
-	    def test_6(self):	
+	    def test_5(self):	
 		existe_clase(editar_perfil)
 	
-	    def test_7(self):	
+	    def test_6(self):	
 		existe_clase(mas_visitadas)
 	
-	    def test_8(self):	
+	    def test_7(self):	
 		existe_clase(rss)
 	
-	    def test_9(self):	
+	    def test_8(self):	
 		existe_clase(highchart)
 	
-	    def test_10(self):	
+	    def test_9(self):	
 		existe_clase(mapa)
 
-   
-
+  
 if __name__ == "__main__":
     unittest.main()
